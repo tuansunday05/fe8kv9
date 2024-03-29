004b1fe47b2ba14e103c5b96d20cd99906312986
Tuan Nguyen Xuan <tuannguyenxuan@192.168.1.5>
2024-03-29 10:36:52 +0700

    add cbamc4

--- models/yolo.py
+++ models/yolo.py
@@ -681,7 +681,7 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
         if m in {
             Conv, AConv, ConvTranspose, 
             Bottleneck, SPP, SPPF, DWConv, BottleneckCSP, nn.ConvTranspose2d, DWConvTranspose2d, SPPCSPC, ADown,
-            RepNCSPELAN4, SPPELAN, CBAMC3, EfficientChannelAttention}:
+            RepNCSPELAN4, SPPELAN, EfficientChannelAttention, ECABottleneck, CBAMBottleneck, CBAMC4}:
             c1, c2 = ch[f], args[0]
             if c2 != no:  # if not output
                 c2 = make_divisible(c2 * gw, 8)
