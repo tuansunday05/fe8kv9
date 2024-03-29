0205b6fbc23a51053695f9b745d29ff1ad5ce286
Tuan Nguyen Xuan <tuannguyenxuan@192.168.1.5>
2024-03-29 08:55:23 +0700

    change e 0.5 -> 1

--- models/common.py
+++ models/common.py
@@ -1301,12 +1301,12 @@ class SpatialAttention(nn.Module):
         return torch.mult(x,out)
 
 class CBAMC3(nn.Module):
-    def __init__(self, c1, c2, c3, c4, n=1, shortcut=True, g=1, e=0.5):
+    def __init__(self, c1, c2, c3, c4, n=1, shortcut=True, g=1, e=1):
         super(CBAMC3, self).__init__()
         c_ = int(c2 * e)
         self.cv1 = Conv(c1, c_, 1, 1)
         self.cv2 = Conv(c1, c_, 1, 1)
-        self.cv3 = Conv(4 * c_, c2, 1)
+        self.cv3 = Conv(2 * c_, c2, 1)
         self.m = nn.Sequential(*[Bottleneck(c_, c_, shortcut, g, e=1.0) for _ in range(n)])
         self.channel_attention = ChannelAttension(c2, 16)
         self.spatial_attention = SpatialAttention(3)
