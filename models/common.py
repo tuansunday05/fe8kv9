aae0da8827bed33696ca7cb7e6c182d828c7511a
Tuan Nguyen Xuan <tuannguyenxuan@192.168.1.5>
2024-03-29 16:10:55 +0700

    add class RepCBAMELAN4, RepCBAM

--- models/common.py
+++ models/common.py
@@ -1211,7 +1211,18 @@ class Classify(nn.Module):
             x = torch.cat(x, 1)
         return self.linear(self.drop(self.pool(self.conv(x)).flatten(1)))
 
+class RepCBAM(nn.Module):
+    # CSP Bottleneck with 3 convolutions
+    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c1, c_, 1, 1)
+        self.cv3 = Conv(2 * c_, c2, 1)  # optional act=FReLU(c2)
+        self.m = nn.Sequential(*(CBAMBottleneck(c_, c_, shortcut, g, e=1.0) for _ in range(n)))
 
+    def forward(self, x):
+        return self.cv3(torch.cat((self.m(self.cv1(x)), self.cv2(x)), 1))
 # ----------------------- Attention Mechanism ---------------------------
 
 ## CBAM ATTENTION
@@ -1314,7 +1325,17 @@ class CBAMC4(nn.Module):
         y = y * self.spatial_attention(y)
         
         return self.cv4(y)
-    
+
+class RepCBAMELAN4(RepNCSPELAN4):
+    # C3 module with CBAMBottleneck()
+    def __init__(self, c1, c2, c3, c4, c5=1): 
+        super().__init__(c1, c2, c3, c4, c5)
+        self.cv2 = nn.Sequential(RepCBAM(c3//2, c4, c5), Conv(c4, c4, 3, 1))
+        self.cv3 = nn.Sequential(RepCBAM(c4, c4, c5), Conv(c4, c4, 3, 1))
+
+        # c_ = int(c2 * e)  # hidden channels
+        # self.m = nn.Sequential(*(RepCBAM(c_, c_, shortcut) for _ in range(n)))
+
 ## GAM ATTETION
 class GAMAttention(nn.Module):
        #https://paperswithcode.com/paper/global-attention-mechanism-retain-information
@@ -1572,4 +1593,34 @@ class LSKAttention(nn.Module):
         return x
 
 
+
+## SE Attention
+class SEBottleneck(nn.Module):
+    # Standard bottleneck
+    def __init__(self, c1, c2, shortcut=True, g=1, e=0.5, ratio=16):  # ch_in, ch_out, shortcut, groups, expansion
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c_, c2, 3, 1, g=g)
+        self.add = shortcut and c1 == c2
+        # self.se=SE(c1,c2,ratio)
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self.l1 = nn.Linear(c1, c1 // ratio, bias=False)
+        self.relu = nn.ReLU(inplace=True)
+        self.l2 = nn.Linear(c1 // ratio, c1, bias=False)
+        self.sig = nn.Sigmoid()
+
+    def forward(self, x):
+        x1 = self.cv2(self.cv1(x))
+        b, c, _, _ = x.size()
+        y = self.avgpool(x1).view(b, c)
+        y = self.l1(y)
+        y = self.relu(y)
+        y = self.l2(y)
+        y = self.sig(y)
+        y = y.view(b, c, 1, 1)
+        out = x1 * y.expand_as(x1)
+
+        # out=self.se(x1)*x1
+        return x + out if self.add else out
 
