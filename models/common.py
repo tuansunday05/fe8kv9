c3c0ced60a2fad0eb3cc48dd0b300fdac774b31c
Tuan Nguyen Xuan <tuannguyenxuan@192.168.1.5>
2024-03-29 12:48:09 +0700

    add LSK Attention class

--- models/common.py
+++ models/common.py
@@ -1519,3 +1519,57 @@ class ECABottleneck(nn.Module):
         out = x1 * y.expand_as(x1)
 
         return x + out if self.add else out
+    
+## LSK Attention
+class LSKblock(nn.Module):
+    def __init__(self, c1):
+        super().__init__()
+        self.conv0 = nn.Conv2d(c1, c1, 5, padding=2, groups=c1)
+        self.conv_spatial = nn.Conv2d(c1, c1, 7, stride=1, padding=9, groups=c1, dilation=3)
+        self.conv1 = nn.Conv2d(c1, c1//2, 1)
+        self.conv2 = nn.Conv2d(c1, c1//2, 1)
+        # self.cv2 = nn.Sequential(RepNCSP(c3 // 2, c4, c5), Conv(c4, c4, 3, 1))
+        self.conv_squeeze = nn.Conv2d(2, 2, 7, padding=3)
+        self.conv = nn.Conv2d(c1//2, c1, 1)
+
+    def forward(self, x):   
+        attn1 = self.conv0(x)
+        attn2 = self.conv_spatial(attn1)
+
+        attn1 = self.conv1(attn1)
+        attn2 = self.conv2(attn2)
+        
+        attn = torch.cat([attn1, attn2], dim=1)
+        avg_attn = torch.mean(attn, dim=1, keepdim=True)
+        max_attn, _ = torch.max(attn, dim=1, keepdim=True)
+        agg = torch.cat([avg_attn, max_attn], dim=1)
+        sig = self.conv_squeeze(agg).sigmoid()
+        attn = attn1 * sig[:,0,:,:].unsqueeze(1) + attn2 * sig[:,1,:,:].unsqueeze(1)
+        attn = self.conv(attn)
+        return x * attn
+
+
+
+class LSKAttention(nn.Module):
+    def __init__(self, c1, c2):
+        super().__init__()
+
+        self.proj_1 = nn.Conv2d(c1, c1, 1)
+        self.activation = nn.GELU()
+        self.spatial_gating_unit = LSKblock(c1)
+        self.proj_2 = nn.Conv2d(c1, c1, 1)
+        self.proj_3 = nn.Conv2d(c1, c2, 1)
+
+
+    def forward(self, x):
+        shorcut = x.clone()
+        x = self.proj_1(x)
+        x = self.activation(x)
+        x = self.spatial_gating_unit(x)
+        x = self.proj_2(x)
+        x = x + shorcut
+        x = self.proj_3(x)
+        return x
+
+
+
