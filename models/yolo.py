90f85ecdc205e792012d6384721dff620e4ae32a
Tuan Nguyen Xuan <tuannguyenxuan@192.168.1.5>
2024-04-01 21:15:26 +0700

    replace Shuffle Attention with LSK Attention

--- models/yolo.py
+++ models/yolo.py
@@ -733,7 +733,7 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--cfg', type=str, default='./detect/attention/yolov9-e-repnsaelan4.yaml', help='model.yaml')
+    parser.add_argument('--cfg', type=str, default='./detect/attention/yolov9-e-lsk.yaml', help='model.yaml')
     parser.add_argument('--batch-size', type=int, default=1, help='total batch size for all GPUs')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--profile', action='store_true', help='profile model speed')
