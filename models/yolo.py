cd362a098b6869475174bff997c6002389c1b2c0
Tuan Nguyen Xuan <tuannguyenxuan@192.168.1.5>
2024-04-02 15:58:52 +0700

    rename repnlskelan4, change to #22,25,28

--- models/yolo.py
+++ models/yolo.py
@@ -681,7 +681,7 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
         if m in {
             Conv, AConv, ConvTranspose, 
             Bottleneck, SPP, SPPF, DWConv, BottleneckCSP, nn.ConvTranspose2d, DWConvTranspose2d, SPPCSPC, ADown,
-            RepNCSPELAN4, SPPELAN, CBAMC4, RepNCBAMELAN4, SOCA, RepNSAELAN4, SABottleneck, LSKBottleneck, RepNLSKLAN4,
+            RepNCSPELAN4, SPPELAN, CBAMC4, RepNCBAMELAN4, SOCA, RepNSAELAN4, SABottleneck, LSKBottleneck, RepNLSKLEAN4,
             RepNECALAN4}:
             c1, c2 = ch[f], args[0]
             if c2 != no:  # if not output
@@ -734,7 +734,7 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--cfg', type=str, default='./detect/attention/yolov9-e-repnecaelan4.yaml', help='model.yaml')
+    parser.add_argument('--cfg', type=str, default='./detect/attention/yolov9-e-repnlskelan4.yaml', help='model.yaml')
     parser.add_argument('--batch-size', type=int, default=1, help='total batch size for all GPUs')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
     parser.add_argument('--profile', action='store_true', help='profile model speed')
